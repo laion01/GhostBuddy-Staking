2e9e8756d3201c03529a464ce2de651961095b3b
c-spider <spdrdev@gmail.com>
2023-03-07 05:48:20 -0800

    remove unused files

--- components/StakingNFTItem/index.jsx
+++ components/StakingNFTItem/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function StakingNFTItem({data}) {
     return (
-        <div className="flex flex-col m-[12px] bg-[url('/images/nftBack.png')] bg-cover pb-[23px] pointer-events-auto">
+        <div className="flex flex-col m-[12px] bg-[url('/images/nftback.png')] bg-cover pb-[23px]">
             <div className="w-[203px] h-[203px] mt-[25px]">
                 <Image alt="" src={data.image} width={200} height={200}/>
             </div>
@@ -16,8 +16,8 @@ export default function StakingNFTItem({data}) {
             </div>
             <p className="mx-[21px] text-[#6C6376] text-[12px]"> 5/7 days staking </p>
             <div className="mx-[21px] flex justify-between items-center">
-                <button className="text-[16px] text-white"> Stake </button>
-                <button className="text-[16px] text-[#91D2F6]"> Claim </button>
+                <button className="text-[16px] text-white pointer-events-auto"> Stake </button>
+                <button className="text-[16px] text-[#91D2F6] pointer-events-auto"> Claim </button>
             </div>
         </div>
     )
